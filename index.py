86b4c5faea53b59af9dabfc530936fd15bb02506
JMarianaStark <marianareyes9@gmail.com>
2020-06-05 18:28:21 -0600

    Se agrego una funcion

--- index.py
+++ index.py
@@ -5,28 +5,28 @@ from datetime import datetime
 
 #Inicio
 raiz = Tk()
-raiz.geometry("500x500")
+raiz.geometry("500x350")
 raiz.title("Examen Final Python Mariana Reyes")
 miFrame= Frame()
 miFrame.pack()
 bienvenido = Label(miFrame, text="...Bienvenidos...")
-bienvenido.grid(row=0, column=0)
-bienvenido.config(font=('Arial', 16))
+bienvenido.grid(row=0, column=0, columnspan=6)
+bienvenido.config(font=('Arial', 20))
 
 
 
-#--Capturando variables
+#Captura de variables 
 nombre = StringVar()
 apellido = StringVar()
 dia = IntVar()
 mes = IntVar()
-anio = IntVar()
+aneo = IntVar()
 
-#--Convertir fecha a binario
 
-#--Retorno de días Vividos
+
+#Operacion dias vividos 
 def contandoDias():
-    fechaString = f"{anio.get()}-{mes.get()}-{dia.get()}"
+    fechaString = f"{aneo.get()}-{mes.get()}-{dia.get()}"
     date_object = datetime.strptime(fechaString, '%Y-%m-%d')
 
     today= datetime.today()
@@ -35,52 +35,38 @@ def contandoDias():
     d2 = date_object
     result1 = abs(d1-d2).days 
 
-    respuesta = f"Usted nacio el {date_object} y ha vivido {result1} días."
+    respuesta = f"Nacio --->{date_object} y ha estado viviendo ---> {result1} días."
 
-    lblResp.configure(text = respuesta)
+    lblResul.configure(text = respuesta)
 
-#--Conteo de Letras de Nombre / Apellido
+#Operancion nombre al revez 
 def contandoLetras():
-    #--concatNombApelli = f"{nombre.get()}{apellido.get()}"
     sNombre = f"{nombre.get()}"
     sApellido = f"{apellido.get()}"
 
     conteoN = len(sNombre)
     conteoA = len(sApellido)
   
-#--Validacion Nombre
     if conteoN % 2 == 0:
-        r1 = f"{sNombre} su Nombre es de número Par"
+        n1 = f"{sNombre} Tu nombre es Par "
     else:
-        r1 = f"{sNombre} su Nombre es de número Inpar"
-#--Validacion Apellido
+        n1 = f"{sNombre} tu nombre es  Inpar"
     if conteoA % 2 == 0:
-        r2 = f"{sApellido} su Apellido es de número Par."
+        a2 = f"{sApellido} tu nombre es  Par."
     else:
-        r2 = f"{sApellido} su Apellido es de número Inpar."
+        a2 = f"{sApellido} tu nombre es  Inpar."
 
-    respuesta = f"{r1} y  {r2} "
+    respuesta = f"{n1} y  {a2} "
 
-    lblResp.configure(text =respuesta )
+    lblResul.configure(text =respuesta )
 
-
-    #--Convertir fecha a binario
-
-#--Retorno de días Vividos
-def conteoDias():
-    fechaString = f"{anio.get()}-{mes.get()}-{dia.get()}"
-    date_object = datetime.strptime(fechaString, '%Y-%m-%d')
-
-    today= datetime.today()
+def alrevez():
+    nene=nombre.get()
     
-    d1 = today
-    d2 = date_object
-    result1 = abs(d1-d2).days 
+    reve=nene[::-1]
 
-    respuesta = f"Usted nacio el {date_object} y ha vivido {result1} días."
 
-    lblResp.configure(text = respuesta)
-
+    lblResul.configure(text =reve)
 
 
 #Creando inputs nombre, apellido, dia, mes, año 
@@ -113,34 +99,43 @@ txtMes=Entry(miFrame, textvariable =mes)
 txtMes.grid(row=4, column=1)
 
 #Input año 
-lblanio=Label(miFrame, text="Año: ")
-lblanio.grid(row=5, column=0)
-lblanio.config(padx=10, pady=10)
-txtanio=Entry(miFrame, textvariable =anio)
-txtanio.grid(row=5, column=1)
+lblaneo=Label(miFrame, text="Año: ")
+lblaneo.grid(row=5, column=0)
+lblaneo.config(padx=10, pady=10)
+txtaneo=Entry(miFrame, textvariable =aneo)
+txtaneo.grid(row=5, column=1)
+
+
 #Botones que se utilizaran para las funciones 
 #Bonton 1 funcion 1
-btnFuncion1 = Button(miFrame, text="Función 1")
-btnFuncion1.grid(row=6, column=0)
-btnFuncion1.config(padx=10, pady=10)
+btnFuncion1 = Button(miFrame, text="Funcion 1")
+btnFuncion1.grid(row=7, column=1)
+btnFuncion1.config(padx=6, pady=6)
 #Bonton 2 funcion 2
-btnFuncion2 = Button(miFrame, text = "Función 2", command=contandoDias)
-btnFuncion2.grid(row=6, column=1)
-btnFuncion2.config(padx=10, pady=10)
+btnFuncion2 = Button(miFrame, text = "Funcion 2", command=contandoDias)
+btnFuncion2.grid(row=7, column=2)
+btnFuncion2.config(padx=6, pady=6)
 #Bonton 3 funcion 
-btnFuncion3 = Button(miFrame, text = "Función 3", command=contandoLetras)
-btnFuncion3.grid(row=7, column=0)
-btnFuncion3.config(padx=10, pady=10)
+btnFuncion3 = Button(miFrame, text = "Funcion 3", command=contandoLetras)
+btnFuncion3.grid(row=7, column=3)
+btnFuncion3.config(padx=6, pady=6)
 #Bonton 4 funcion 4
-btnFuncion4 = Button(miFrame, text = "Función 4")
-btnFuncion4.grid(row=7, column=1)
-btnFuncion4.config(padx=10, pady=10)
+btnFuncion4 = Button(miFrame, text = "Funcion 4")
+btnFuncion4.grid(row=7, column=4)
+btnFuncion4.config(padx=6, pady=6)
 #Bonton 5 funcion 5
-btnFuncion5 = Button(miFrame, text = "Función 5")
-btnFuncion5.grid(row=8, column=0)
-btnFuncion5.config(padx=10, pady=10)
+btnFuncion5 = Button(miFrame, text = "Funcion 5", command=alrevez)
+btnFuncion5.grid(row=7, column=5)
+btnFuncion5.config(padx=6, pady=6)
 
 
+#Funcion de Respuestas: Aqui se reacciona 
+lblResul=Label(miFrame, text="La respuesta---->")
+lblResul.grid(row=9, column=0)
+lblResul.config(padx=10, pady=10)
+lblR=Label(miFrame)
+lblR.grid(row=10, column=0)
+lblR.config(padx=10, pady=10)
 
 raiz.mainloop()
 
